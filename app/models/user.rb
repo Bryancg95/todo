e2b8f970d43e28666e47499615f0ded58bf3cbd3
Bryancg95 <litcbryancg@hotmail.com>
2018-10-19 20:18:03 -0500

    Mail and corrections were added in user edition

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :todo_list
 
-  
+
 
   has_attached_file :avatar, styles: { medium: "300x300", thumb: "100x100" }
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
